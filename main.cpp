41cf19f05a8154da7bc50003bca66ececcdeaade
blacElsa <newp1rat@mail.ru>
2020-09-28 21:24:51 +0200

    add file functions

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <ctime>
+#include <cstdio>
 #include <random>
 #include <string>
 #include <iostream>
@@ -5,6 +7,9 @@
 #include <string_view>
 
 #include <fmt/core.h>
+#include <fmt/chrono.h>
+
+#define PASSWORD_FILE_NAME "password.txt"
 
 std::string make_password(std::string_view alphabet, int length)
 {
@@ -22,12 +27,27 @@ std::string make_password(std::string_view alphabet, int length)
     return password;
 }
 
+int create_password_file()
+{
+    auto file{std::fopen(PASSWORD_FILE_NAME, "w")};
+    if (!file) return 0; // file open failed
+    fmt::print(file, "{0:<32}|{1:^15}|{2:^10}\n", "Password", "Service", "Date");
+    fmt::print(file, "{0:-^32}+{0:-^15}+{0:-^10}\n", "");
+    std::fclose(file);
+    return 1;
+}
+
+int write_password_to_file(std::string_view password, std::string_view service)
+{
+    auto file{std::fopen(PASSWORD_FILE_NAME, "a")};
+    if (!file) return 0;
+    fmt::print(file, "{0:<32}|{1:^15}|{2:%Y-%m-%d}\n", password, service, fmt::localtime(std::time(nullptr)));
+    std::fclose(file);
+    return 1;
+}
+
 int main()
 {
-    while (true) {
-        auto password{make_password("1234567890", 4)};
-        std::cout << fmt::format("{}\n", password);
-        std::cin.get();
-    }
+    
     return 0;
 }
